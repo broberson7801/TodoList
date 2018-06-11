d3baa47f068ff1437b2cf34a6d0321440259ba8c
broberson7801 <broberson7801@gmail.com>
2018-06-11 05:04:19 -0600

    added annotation to talk to node

--- src/main/java/controllers/TodoController.java
+++ src/main/java/controllers/TodoController.java
@@ -5,6 +5,7 @@ import static util.Constants.TODO_PATH;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import util.Constants;
 
 @RestController
 @RequestMapping(value=TODO_PATH)
+@CrossOrigin(origins = "http://localhost:4200")
 public class TodoController {
 	
 	@Autowired
